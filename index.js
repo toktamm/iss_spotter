0e7e6e74c04ad5cca4733afd686f7221cc915e5b
Toktam <ttmohebbi@gmail.com>
2020-09-24 23:56:26 +0000

    commented out the temporary code in index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // index.js
-const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// const { fetchMyIP } = require('./iss');
 
-  console.log('It worked! Returned IP:' , ip);
-});
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
