12f58585c339db66dcc4efdc66ef1f845d0107f9
Toktam <ttmohebbi@gmail.com>
2020-09-25 01:05:17 +0000

    defined fetchCoordsByIP

--- index.js
+++ index.js
@@ -11,3 +11,14 @@
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
+const { fetchCoordsByIP } = require('./iss');
+//we need 2 arguements: the ip address and the callback
+fetchCoordsByIP('76.68.115.71', (error, coordinates) => {
+    if (error) {
+      console.log("It didn't work!" , error);
+      return;
+    }
+  
+    console.log('It worked! Returned IP:' , coordinates);
+  });
+
--- iss.js
+++ iss.js
@@ -31,9 +31,33 @@ const fetchMyIP = function(callback) {
     const ip = JSON.parse(body).ip;
     callback(null, ip);
 
+  })
+}
+
+
+const fetchCoordsByIP = function(ip, callback) {
+
+  request('https://ipvigilante.com/' + ip, (error, response, body) => {
+
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const latitude = JSON.parse(body).data.latitude;
+    const longitude = JSON.parse(body).data.longitude;
+    // console.log(JSON.parse(body));
+    callback(null, { latitude, longitude });
 
   })
 }
 
-module.exports = { fetchMyIP };
 
+module.exports = { fetchMyIP, fetchCoordsByIP };
